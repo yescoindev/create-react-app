d2298db9a590af834a6d1ddccaadd8d760e80a52
Thien <thien@quoine.com>
2016-10-19 03:28:31 +0700

    add ignore order for extract text webpack

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -241,7 +241,9 @@ module.exports = {
       }
     }),
     // Note: this won't work without ExtractTextPlugin.extract(..) in `loaders`.
-    new ExtractTextPlugin('static/css/[name].[contenthash:8].css'),
+    new ExtractTextPlugin('static/css/[name].[contenthash:8].css', {
+      ignoreOrder: true
+    }),
     // Generate a manifest file which contains a mapping of all asset filenames
     // to their corresponding output file so that tools can pick it up without
     // having to parse `index.html`.
