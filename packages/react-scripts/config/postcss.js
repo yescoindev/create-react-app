200cd3dfb876add8ee0af97a40775342524fb1df
Thien <thien@quoine.com>
2016-11-27 15:26:11 +0700

    add purple theme

--- packages/react-scripts/config/postcss.js
+++ packages/react-scripts/config/postcss.js
@@ -2,7 +2,14 @@ function getPostCSSConfig() {
   return [
     require('postcss-import'),
     require('postcss-global-theme')({
-      themes: ['dark', 'dark-2', 'finance', 'green', 'yellow'] // TODO: this should be set in css file
+      themes: [
+        'dark',
+        'dark-2',
+        'finance',
+        'green',
+        'yellow',
+        'purple',
+      ] // TODO: this should be set in css file
     }),
     require('postcss-simple-vars'),
     require('postcss-calc'),
