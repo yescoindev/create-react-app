0927b663954d414b1af76819bf5de8a8fa80a58d
Thien <dvkndn@gmail.com>
2016-12-08 21:06:27 +0700

    fix

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -111,10 +111,7 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         loader: 'eslint',
-        include: [
-          paths.appSrc,
-          paths.appQuoineModules
-        ],
+        include: paths.appSrc,
       }
     ],
     loaders: [
