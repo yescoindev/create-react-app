24c093b7b28888074086cfda4f8bb883d3d10bb3
Thien <dvkndn@gmail.com>
2017-01-01 21:56:23 +0700

    temporary disable webpack resolver in eslint

---
+++ packages/react-scripts/.eslintrc.js
@@ -0,0 +1,13 @@
+{
+  "extends": "airbnb",
+  "env": {
+    "browser": true
+  },
+  "rules": {
+    // temporary disable these rules to allow custom webpack config in
+    // "resolve". TODO: use webpack resolver properly
+    "import/no-extraneous-dependencies": 0,
+    "import/no-unresolved": 0,
+    "import/extensions": 0
+  }
+}
